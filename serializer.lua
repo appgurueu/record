f5daf7cf0c9fc004abba1c4ae62901a8ca39789d
Lars Mueller <appgurulars@gmx.de>
2025-08-29 13:39:59 +0200

    Switch serialization to modlib's bluon

--- serializer.lua
+++ serializer.lua
@@ -1,5 +1,20 @@
--- We will likely want to replace this at some point
-return {
-	deserialize = core.deserialize,
-	serialize = core.serialize,
-}
+local bluon = modlib.bluon.new()
+
+local serializer = {}
+
+local function stream_write(stream, str)
+	table.insert(stream, str)
+end
+
+function serializer.serialize(value)
+	local stream = {write = stream_write}
+	bluon:write(value, stream)
+	return table.concat(stream)
+end
+
+function serializer.deserialize(str)
+	local value = bluon:read(modlib.text.inputstream(str))
+	return value
+end
+
+return serializer
